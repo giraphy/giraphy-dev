dc280a9bdb1038729c971a916288c6afcf120803
YuitoSato <yuitosato.w@gmail.com>
2020-08-29 14:55:39 +0900

    :+1: update database setting

---
+++ base-schema.ts
@@ -0,0 +1,4 @@
+import { GraphQLFieldConfig, GraphQLInt, GraphQLList, GraphQLSchema, GraphQLString } from 'graphql';
+import { executeQuery, GiraphyObjectType } from 'giraphy';
+import * as SqlString from 'sqlstring';
+
